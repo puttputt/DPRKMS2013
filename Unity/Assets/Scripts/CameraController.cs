47770b43463a9623b7059db74ceb9107cc758e7b
Kyle Smyth <kylesmyth@gmail.com>
2013-02-18 13:45:48 -0600

    Added silo to project

--- Unity/Assets/Scripts/CameraController.cs
+++ Unity/Assets/Scripts/CameraController.cs
@@ -12,11 +12,11 @@ public class CameraController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        this.transform.LookAt(this.objectToFollow.transform);
+        //this.transform.LookAt(this.objectToFollow.transform);
         this.transform.position = new Vector3(
             this.objectToFollow.transform.position.x, 
-            this.objectToFollow.transform.position.y - 2, 
-            this.objectToFollow.transform.position.z - 10
+            this.objectToFollow.transform.position.y + 8, 
+            this.objectToFollow.transform.position.z - 15
             );
 	}
 }
