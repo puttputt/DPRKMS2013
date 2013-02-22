e880841542957c2f994210829f624601ac54c618
Kyle Smyth <kylesmyth@gmail.com>
2013-02-21 20:12:35 -0600

    Engine particles now play when W is pressed and stop when it is not.
    Fins can fall off.

---
+++ Unity/Assets/Scripts/FinLauncher.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using System.Collections;
+
+public class FinLauncher : MonoBehaviour {
+
+	private bool launched = false;
+	const float gravConst=-0.5f;
+	private Vector3 totalTorque;
+	
+	public void Start()
+	{
+		this.totalTorque = new Vector3(0f, 0f, 0f);	
+	}
+	
+	public void FixedUpdate()
+	{
+		if(this.launched)
+		{
+			
+			Vector3 rocket_pos = this.transform.position;
+			Vector3 grav_dir = new Vector3(0.0f, 0.0f, 0.0f);
+			if(rocket_pos.magnitude !=0)
+				grav_dir = gravConst*(rigidbody.mass)*(rocket_pos)/rocket_pos.magnitude;
+			rigidbody.AddForce(grav_dir, ForceMode.Impulse);
+			
+			this.AccelerateTorque();
+		}
+	}
+	
+	public void launch(Rigidbody rocketBody)
+	{
+		if(!this.launched)
+		{
+			this.totalTorque += this.transform.right * 10;
+			this.launched = true;
+		}
+	}
+	
+	private void AccelerateTorque()
+	{
+		this.rigidbody.AddRelativeTorque(this.totalTorque * 25, ForceMode.Force);
+	}
+}
