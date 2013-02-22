df9fbbcce74de4b1f8aba3c21ec2d0c5579367b4
landonr <landonroha@gmail.com>
2013-02-21 23:51:44 -0600

    made the rocket cute new fins

--- Unity/Assets/Scripts/EngineController.cs
+++ Unity/Assets/Scripts/EngineController.cs
@@ -1,32 +1,38 @@
 using UnityEngine;
 using System.Collections;
 
-public class EngineController : MonoBehaviour 
+public class EngineController : MonoBehaviour
 {
+	public ParticleSystem launchSmoke;
+	public ParticleSystem flightSmoke;
+	public ParticleSystem flightFlames;
+	public GameObject Rocket;
+	public int disableSmokeHeight;
 
-    private ParticleSystem[] particles;
-
-    private void Start()
-    {
-		this.particles = this.gameObject.GetComponentsInChildren<ParticleSystem>();
-    }
-
-    public void play()
-    {
-        this.light.gameObject.SetActive(true);
-		foreach(ParticleSystem ps in this.particles)
-		{
-			Debug.Log(ps.name);
-			ps.Play();
+	private void Start ()
+	{
+	}
+	
+	public void Update ()
+	{
+		if (this.Rocket.transform.position.y > this.disableSmokeHeight) {
+			//launchSmoke.Stop ();
 		}
-    }
+	}
 
-    public void stop()
-    {
-        this.light.gameObject.SetActive(false);
-        foreach(ParticleSystem ps in this.particles)
-		{
-			ps.Stop();
-		}
-    }
+	public void play ()
+	{
+		this.light.gameObject.SetActive (true);
+		flightSmoke.Play ();
+		flightFlames.Play ();
+		//if (this.Rocket.transform.position.y < this.disableSmokeHeight)
+		launchSmoke.Play ();
+	}
+
+	public void stop ()
+	{
+		this.light.gameObject.SetActive (false);
+		flightSmoke.Stop ();
+		flightFlames.Stop ();
+	}
 }
